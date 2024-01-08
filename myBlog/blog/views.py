bcfa273aa3fe3263267dfba92c3b3efb5144a63d
nhanteefu <22028114@vnu.edu.vn>
2024-01-08 23:41:07 +0700

    hehe

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -10,4 +10,9 @@ def index(request):
         'quote': 'gambatte'
     }
     return render(request, 'index.html', dic)
+
+def counter(request):
+    words = request.GET['words']   #name = 'words' in index.html
+    lengthOfWords = len(words.split())
+    return render(request, 'counter.html', {'length': lengthOfWords, 'words': words})
     
