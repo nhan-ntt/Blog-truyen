b5b5ff493834e55f79b70629e5a393d8017ea13f
nhanteefu <22028114@vnu.edu.vn>
2024-01-09 14:40:50 +0700

    change project's name

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 
 def index(request):
     # return HttpResponse('<h1>Blog Home</h1>')
 
-    return render(request, 'index.html')
+    features = Feature.objects.all()
+
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
     words = request.POST['words']   #name = 'words' in index.html
